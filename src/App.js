b319643d1f3c51857e2346fccb4364422e9a2ec7
bijayneupane18 <bjneupane5@gmail.com>
2022-07-05 13:53:59 +0545

    work on all

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Navbar';
+ 
+function App() {
+  return (
+    <div className='app'>
+      <img className='app-bg'
+      src='https://wallpapercave.com/wp/VYGMAH5.jpg'
+      alt='background'
+      />
+      <Navbar/>
+      {/* <img className='app-bg'
+      src='https://wallpapercave.com/wp/VYGMAH5.jpg'
+      alt='background'
+      />
+      <img className='app-bg'
+      src='https://wallpapercave.com/wp/VYGMAH5.jpg'
+      alt='background'
+      />
+      <img className='app-bg'
+      src='https://wallpapercave.com/wp/VYGMAH5.jpg'
+      alt='background'
+      /> */}
+    </div>
+  );
+}
+
+export default App;
